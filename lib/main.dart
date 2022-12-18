cbb3d54f3abf0e63d2afa85499f9b42a4c1db7d8
AndreMaulanaMustofa <andremm73@gmail.com>
2022-12-18 16:26:20 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:uas_kelompok1_mobile/pages/biodata_page.dart';
+import 'package:uas_kelompok1_mobile/pages/data_page.dart';
+import 'package:uas_kelompok1_mobile/pages/detail_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/' :(context) => BiodataPage(),
+      '/data' :(context) => DataPage(),
+      '/detail' :(context) => DetailPage(),
+    },
+  ));
+}
