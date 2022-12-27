8d8f8410d7e32606659106c9fea68820bd2c0f91
AndreMaulanaMustofa <andremm73@gmail.com>
2022-12-27 09:33:50 +0700

    template tiara

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uas_kelompok1_mobile/DbHelper.dart';
 import 'package:uas_kelompok1_mobile/models/item.dart';
+import 'package:uas_kelompok1_mobile/pages/data_page.dart';
 
 class DetailPage extends StatelessWidget{
 
@@ -8,7 +11,187 @@ class DetailPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Detail Page"),
+        ),
+        body: myBody(),
+      ),
     );
   }
+}
+class myBody extends StatefulWidget{
+  @override
+  State<myBody> createState() => _myBody();
+}
+
+enum Gender { male , female}
+
+final nimController = TextEditingController();
+final namaController = TextEditingController();
+final alamatController = TextEditingController();
+
+class _myBody extends State<myBody>{
+
+  Item item;
+
+  Gender _gender = Gender.male;
+
+  DbHelper dbHelper;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Text(
+              "Edit Biodata",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 20),
+          ),
+          Container(
+            child: TextField(
+              decoration: InputDecoration(
+                label: Text("NIM : "),
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              controller: nimController,
+            ),
+            margin: EdgeInsets.only(
+              left: 100,
+              right: 100,
+            ),
+          ),
+          Container(
+            child: TextField(
+              decoration: InputDecoration(
+                label: Text("Nama :"),
+              ),
+              keyboardType: TextInputType.name,
+              controller: namaController,
+            ),
+            margin: EdgeInsets.only(
+              left: 100,
+              right: 100,
+            ),
+          ),
+          Container(
+            child: TextField(
+              decoration: InputDecoration(label: Text("Alamat :")),
+              keyboardType: TextInputType.streetAddress,
+              controller: alamatController,
+            ),
+            margin: EdgeInsets.only(
+              left: 100,
+              right: 100,
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: RadioListTile(
+                  title: const Text("Male"),
+                  value: Gender.male,
+                  groupValue: _gender,
+                  onChanged: (Gender value) {
+                    setState(() {
+                      _gender = value;
+                    });
+                  },
+                  contentPadding:
+                      EdgeInsets.only(left: 10, top: 20, bottom: 20),
+                ),
+              ),
+              Expanded(
+                child: RadioListTile(
+                  title: const Text("Female"),
+                  value: Gender.female,
+                  groupValue: _gender,
+                  onChanged: (Gender value) {
+                    setState(() {
+                      _gender = value;
+                    });
+                  },
+                  contentPadding:
+                      EdgeInsets.only(top: 20, bottom: 20),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  var gender = _gender.toString();
+
+                  if (gender == "Gender.male") {
+                    gender = "male";
+                  } else {
+                    gender = "female";
+                  }
+
+                  item = Item(int.parse(nimController.text),
+                      namaController.text, alamatController.text, gender);
+                  if (item != null) {
+                    print('goto here');
+                    await editItem(item);
+                    if (!mounted) return;
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DataPage(),
+                      ),
+                    );
+                  }
+                },
+                child: Text("Tambah Data"),
+                style: ButtonStyle(
+                  alignment: Alignment.center,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+  Future<void> editItem(Item item) async {
+    print('goto here2');
+    int result = await DbHelper.update(item);
+    if (!mounted) return;
+    if (result > 0) {
+      showAlertDialog(context);
+    } 
+  }
+}
+
+showAlertDialog(BuildContext context) {
+  Item item;
+  Widget okButton = MaterialButton(
+    child: Text("OK"),
+    onPressed: () {},
+  );
+
+  AlertDialog alert = AlertDialog(
+    title: Text("Success"),
+    content: Text("Data Telah di Update"),
+    actions: [
+      okButton,
+    ],
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
